53c3b7e275f86b69dd5aace6827ae1ba8ab4f7f5
palomajurado <palomania@live.com>
2020-09-14 01:53:07 -0500

    Chart for employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+// esta clase es exclusiva para devolver objetos
 export class AppComponent {
-  title = 'simple-crud-angular';
+  simpleAlert() {
+    Swal.fire('Hello world!');
+  }
+
+  alertWithSuccess() {
+    Swal.fire('Thank you...', 'You submitted succesfully!', 'success');
+  }
+
+  confirmBox() {
+    Swal.fire({
+      title: 'Are you sure want to remove?',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it',
+    }).then((result) => {
+      console.log(result);
+      if (result.value) {
+        this.employeeArray = this.employeeArray.filter(
+          (employee) => employee != this.selectedEmployee
+        );
+        this.selectedEmployee = new Employee();
+        Swal.fire('Deleted!', 'success');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled');
+      }
+    });
+  }
+
+  employeeArray: Employee[] = [
+    { id: 1, name: 'Juana', country: 'Bolivia' },
+    { id: 2, name: 'Andrea', country: 'Bolivia' },
+    { id: 3, name: 'Alberto', country: 'Bolivia' },
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit() {
+    if (this.selectedEmployee.id === 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+  openForEDit(employee: Employee) {
+    this.selectedEmployee = employee;
+  }
 }
